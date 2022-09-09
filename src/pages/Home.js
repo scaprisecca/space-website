83d5a69d7096f0912a9db456c28855ff31a92a72
Scott Caprisecca <caprisecca@gmail.com>
2022-09-08 21:31:54 -0700

    added pages

---
+++ src/pages/Home.js
@@ -0,0 +1,19 @@
+const Home = () => {
+    return ( 
+        <div class="home__content-wrapper">
+            <div class="home__content">
+                <h5 class="home__subheading">So, you want to travel to</h5>
+                <h1 class="home__heading">SPACE</h1>
+                <p class="home__text">Let's face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we'll give you a truly out of this world experience!</p>
+            </div>
+
+            <div class="home__button--hover hidden">
+                <button class="home__button visible">
+                    <p>EXPLORE</p>
+                </button>
+            </div>
+        </div>
+     );
+}
+ 
+export default Home;
